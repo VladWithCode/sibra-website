62ae9e977ac32a6b7350591f36bba020ec760235
vladwithcode <vladwithcode@gmail.com>
2022-10-08 12:49:30 -0600

    [Change] Temporary dev home fetching

--- src/services/HomeService.js
+++ src/services/HomeService.js
@@ -1,8 +1,8 @@
 export async function fetchHomes() {
   let url =
-    process.env.NODE_ENV === 'production'
+    /* process.env.NODE_ENV === 'production'
       ? '/api/homes'
-      : 'https://mockend.com/vladwithcode/sibra-website/homes?limit=10';
+      : */ 'https://mockend.com/vladwithcode/sibra-website/homes?limit=10';
 
   const response = await fetch(url);
 
