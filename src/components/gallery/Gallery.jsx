5c73550cffce6e315dc88b6171c86780e96919dd
vladwithcode <vladwithcode@gmail.com>
2022-10-08 12:47:27 -0600

    [Fix] add missing key prop

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -5,7 +5,7 @@ function Gallery({ homes }) {
   return (
     <div className='gallery flex items-center space-x-4 h-96 py-4 overflow-x-scroll overflow-y-hidden'>
       {homes?.map(home => (
-        <GalleryCard home={home} />
+        <GalleryCard home={home} key={home.id} />
       ))}
     </div>
   );
