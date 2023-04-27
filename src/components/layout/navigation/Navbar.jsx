e4ec443d4db1e5691706768e83a2ffc91a6fe973
VladWithCode <vladwithcode@gmail.com>
2023-04-26 22:27:38 -0600

    Change color to green

--- src/components/layout/navigation/Navbar.jsx
+++ src/components/layout/navigation/Navbar.jsx
@@ -4,23 +4,23 @@ import Menu from './Menu';
 import Navigation from './Navigation';
 
 function Navbar({}) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  return (
-    <nav className='flex-1 flex items-center px-4 justify-between'>
-      <Link href='/'>
-        <a className='text-2xl font-bold pr-8'>Sibra</a>
-      </Link>
-      {!isMenuOpen ? <Navigation /> : ''}
-      {/* <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} /> */}
-      <div
-        className='space-y-1.5 w-6 md:ml-8 cursor-pointer'
-        onClick={() => setIsMenuOpen(prev => !prev)}>
-        <div className='h-0.5 w-full bg-emerald-400'></div>
-        <div className='h-0.5 w-full bg-emerald-400'></div>
-        <div className='h-0.5 w-4 bg-emerald-400'></div>
-      </div>
-    </nav>
-  );
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	return (
+		<nav className="flex-1 flex items-center px-4 justify-between">
+			<Link href="/">
+				<a className="text-2xl font-bold pr-8">Sibra</a>
+			</Link>
+			{!isMenuOpen ? <Navigation /> : ''}
+			{/* <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} /> */}
+			<div
+				className="space-y-1.5 w-6 md:ml-8 cursor-pointer"
+				onClick={() => setIsMenuOpen(prev => !prev)}>
+				<div className="h-0.5 w-full bg-green-600"></div>
+				<div className="h-0.5 w-full bg-green-600"></div>
+				<div className="h-0.5 w-4 bg-green-600"></div>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
